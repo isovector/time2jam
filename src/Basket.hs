89c8f8a7234f059c6c4e5dfe58225d8082fac879
Sandy Maguire <sandy@sandymaguire.me>
2017-01-06 14:44:10 -0800

    center line

--- src/Basket.hs
+++ src/Basket.hs
@@ -1,10 +1,7 @@
-{-# LANGUAGE RankNTypes #-}
-
 module Basket where
 
 import Camera
 import Constants
-import Control.Lens ((+~), Lens')
 import Game.Sequoia.Color
 import Game.Sequoia.Utils
 import JamPrelude
@@ -12,8 +9,7 @@ import JamPrelude
 
 drawBasket :: Camera -> Net -> Form
 drawBasket cam net =
-    group [ threePointLine cam net
-          , move (toScreen cam $ basketPos net)
+    group [ move (toScreen cam $ basketPos net)
             . filled (rgb 0.13 0.13 0.13)
             $ billboard cam
                         (basketPos net)
@@ -27,34 +23,6 @@ drawBasket cam net =
   where
     netWidth = 40
     netHeight = 20
-
-arc3d :: Int
-      -> V3
-      -> Double
-      -> Double
-      -> Double
-      -> Lens' V3 Double
-      -> Lens' V3 Double
-      -> [V3]
-arc3d samples pos r a1 a2 x y =
-    fmap (rad2rel . getRad . fromIntegral) [0..samples]
-  where
-    drad = (a2 - a1) / fromIntegral samples
-    getRad i = drad * i + a1
-    rad2rel rad = pos & x +~ cos rad * r
-                      & y +~ sin rad * r
-
-threePointLine :: Camera -> Net -> Form
-threePointLine cam n = traced' red
-                     . polygon
-                     . fmap (toScreen cam)
-                     $ arc3d 20 pos courtLongRadius a1 a2 _x _z
-  where
-    pos = basketGroundPos n
-    mid = toScreen cam pos
-    (a1, a2) = case n of
-                 LNet -> (pi*3/2, pi*5/2)
-                 RNet -> (pi/2, pi*3/2)
 
 basketPos :: Net -> V3
 basketPos n = courtLength / 2 *^ fwd
--- src/Court.hs
+++ src/Court.hs
@@ -1,9 +1,14 @@
+{-# LANGUAGE RankNTypes #-}
+
 module Court where
 
-import Game.Sequoia
-import Types
-import Constants
+import Basket
 import Camera
+import Constants
+import Control.Lens ((+~), Lens')
+import Game.Sequoia
+import Game.Sequoia.Color
+import JamPrelude
 
 data Court = Court
   { _courtTopLeft  :: !V3
@@ -22,13 +27,53 @@ court = Court (V3 (-width) 0 (-depth))
     depth = courtDepth / 2
 
 drawCourt :: Court -> Camera -> Form
-drawCourt c cam = move (toScreen cam $ V3 0 0 0)
-                . filled (rgb 1 0.67 0.47)
-                . toPoly (toScreen cam $ V3 0 0 0)
-                $ fmap (toScreen cam)
-                [ _courtTopLeft  c
-                , _courtTopRight c
-                , _courtBotRight c
-                , _courtBotLeft  c
-                ]
+drawCourt c cam =
+  group [ filled (rgb 1 0.67 0.47)
+          . polygon
+          $ fmap (toScreen cam)
+          [ _courtTopLeft  c
+          , _courtTopRight c
+          , _courtBotRight c
+          , _courtBotLeft  c
+          ]
+        , threePointLine cam LNet
+        , threePointLine cam RNet
+        , outlined' red
+          . polygon
+          $ fmap (toScreen cam)
+          [ V3 0 0 (-courtDepth / 2)
+          , V3 0 0 (courtDepth / 2)
+          ]
+        , outlined' red
+          . polygon
+          . fmap (toScreen cam)
+          $ arc3d 40 (V3 0 0 0) 1.8 0 (2 * pi) _x _z
+        ]
+
+arc3d :: Int
+      -> V3
+      -> Double
+      -> Double
+      -> Double
+      -> Lens' V3 Double
+      -> Lens' V3 Double
+      -> [V3]
+arc3d samples pos r a1 a2 x y =
+    fmap (rad2rel . getRad . fromIntegral) [0..samples]
+  where
+    drad = (a2 - a1) / fromIntegral samples
+    getRad i = drad * i + a1
+    rad2rel rad = pos & x +~ cos rad * r
+                      & y +~ sin rad * r
+
+threePointLine :: Camera -> Net -> Form
+threePointLine cam n = outlined' red
+                     . polygon
+                     . fmap (toScreen cam)
+                     $ arc3d 20 pos courtLongRadius a1 a2 _x _z
+  where
+    pos = basketGroundPos n
+    (a1, a2) = case n of
+                 LNet -> (pi*3/2, pi*5/2)
+                 RNet -> (pi/2, pi*3/2)
 
